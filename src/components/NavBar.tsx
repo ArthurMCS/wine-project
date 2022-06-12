745ed19f0326cee0a44714d3fad896e887d0ac39
Arthur Moreira <arthurmcs96@gmail.com>
2022-06-12 00:00:10 -0300

    feat: cria a logica de pesquisa pelo nome do vinho

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,13 +1,14 @@
 /* eslint-disable import/no-unresolved */
 /* eslint-disable import/extensions */
 /* eslint-disable react/jsx-filename-extension */
-import React, { useState } from 'react';
+import React, { useState, useContext } from 'react';
 import { IoMdSearch } from 'react-icons/io';
+import AppContext from '../context/AppContext';
 import NavBarStyledComponent from '../styles/NavBar';
 
 export default function NavBar() {
   const [displaySearchBar, setDisplaySetBar] = useState(false);
-  const [search, setSearch] = useState('');
+  const { search, setSearch } = useContext(AppContext);
 
   return (
     <NavBarStyledComponent>
@@ -19,7 +20,7 @@ export default function NavBar() {
         displaySearchBar && (
         <input
           type="text"
-          onChange={(e) => setSearch(e.target.value)}
+          onChange={(e) => setSearch(e.target.value.toLocaleLowerCase().trim())}
           value={search}
           placeholder="Procure seu vinho"
           className="input"
--- src/context/AppProvider.jsx
+++ src/context/AppProvider.jsx
@@ -6,23 +6,38 @@ import AppContext from './AppContext';
 export default function AppProvider({ children }) {
   const [wines, setWines] = useState([]);
   const [cart, setCart] = useState([]);
+  const [search, setSearch] = useState('');
 
   const fetchWines = async () => {
     const res = await axios.get('http://localhost:3000/api/wines');
     const items = await res.data;
-    return items;
+    setWines(items.wines);
   };
 
   useEffect(() => {
-    fetchWines().then((result) => setWines(result.wines));
+    fetchWines();
     const CartLs = JSON.parse(localStorage.getItem('cart'));
     if (CartLs) {
       setCart(CartLs);
     }
   }, []);
 
+  useEffect(() => {
+    if (search.length > 3) {
+      const winesFiltered = wines.filter((wine) => wine.name.toLowerCase().includes(search));
+      setWines(winesFiltered);
+    }
+
+    if (search.length < 3 && search.length > 0) {
+      fetchWines();
+    }
+  }, [search]);
+
   return (
-    <AppContext.Provider value={{ wines, cart, setCart }}>
+    <AppContext.Provider value={{
+      wines, cart, setCart, search, setSearch,
+    }}
+    >
       {children}
     </AppContext.Provider>
   );
